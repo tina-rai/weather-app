004b9095851c7b90985f8da86fe99ecbd12f0319
Tina <contactina101@gmail.com>
2026-07-31 19:19:43 +0545

    Add dark mode

--- script.js
+++ script.js
@@ -29,6 +29,25 @@ document.getElementById("location-btn");
 
 locationButton.addEventListener("click", getCurrentLocation);
 
+const themeToggle = document.getElementById("theme-toggle");
+
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+    themeToggle.textContent = "☀️ Light Mode";
+}
+
+themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    const dark = document.body.classList.contains("dark");
+
+    localStorage.setItem("theme", dark ? "dark" : "light");
+
+    themeToggle.textContent = dark
+        ? "☀️ Light Mode"
+        : "🌙 Dark Mode";
+});
+
 form.addEventListener("submit", async function (event) {
     event.preventDefault();
 
